c0c4e6a5957dbf5a5611e1139aa7d0475232ec6e
ivanichiki <62132967+ivanichiki@users.noreply.github.com>
2020-03-22 20:37:05 +0300

    secossdnssd

--- src/components/OtherPage/OtherPage.jsx
+++ src/components/OtherPage/OtherPage.jsx
@@ -48,18 +48,28 @@ export const OtherPage = ({ match }) => {
       {!moviedata ? <div className='preloader' style={{ width: '990px' }}> <SVGLoaders.Bars /> </div> :
         <div className='wr'>
           <div className='filmHeader'>
-            <div className='mtitle'>
+            <div className='column'>
+              <div className='mtitle'>
 
-              {moviedata.title}
-            </div>
-            <div className='date'>
+                {moviedata.title}
+              </div>
+              <div className='date'>
 
-              {`(${moviedata.release_date.slice(0,-6)})`}
+                {`(${moviedata.release_date.slice(0, -6)})`}
+              </div>
             </div>
+            <div className='column'>
+              <div className='star'>&#9733;</div>
+              <div > <div> <span className='rate'> {moviedata.vote_average} </span> <span className='podsos'>/10</span></div> 
+              <div> <span className='count'>{moviedata.vote_count}  </span> </div>
+              </div>
+            </div>
+
+
           </div>
           <img src={`https://image.tmdb.org/t/p/w500${moviedata.poster_path}`} />
 
-          {!results ? <div className='preloader' style={{ width: '990px' }}> <SVGLoaders.Bars /> </div> : <iframe width="900px" height="515" src={`https://www.youtube-nocookie.com/embed/${results}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}
+          {!results ? <div className='preloader' style={{ width: '990px' }}> <SVGLoaders.Bars /> </div> : <iframe width="980px" height="515" src={`https://www.youtube-nocookie.com/embed/${results}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}
           <h1>About Film</h1>
           <div className='text'>{moviedata.overview} </div>
 
